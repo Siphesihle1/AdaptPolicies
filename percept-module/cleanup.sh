b05659b26d7ef342fa3c601f25d3a7cf72ac9559
Siphesihle Mthethwa <percysiphe@gmail.com>
2025-11-23 17:32:45 +0200

    Initial commit with just the simulator

--- percept-module/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker compose down
-docker container rm percept_module
-docker image rm percept_module:latest
---
+++ src/agent.py
@@ -0,0 +1,24 @@
+# %
+from alfred_thor_env import AlfredThorEnv
+from pprint import pprint
+
+# %%
+env = AlfredThorEnv(headless=False)
+task = env.load_next_task_scene()
+env.stop()
+
+# %%
+if env.initial_event is not None:
+    metadata = env.initial_event.metadata
+    objects = metadata["objects"]
+    detected_objects = env.initial_event.instance_detections2D
+
+    pprint(task)
+
+    pprint([obj["objectId"] for obj in objects if obj["visible"]])
+    pprint(f"Number of object in the scene: {len(objects)}")
+
+    if detected_objects:
+        pprint([*detected_objects.keys()])
+        pprint(f"Number of detected objects: {len(detected_objects.keys())}")
+# %%
