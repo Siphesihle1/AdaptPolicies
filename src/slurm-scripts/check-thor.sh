30094638dbd0327d433575469727450d9c9f51fa
Siphesihle Mthethwa <siphesihlemthetwa@M-BBD-SipM.local>
2026-01-03 22:31:53 +0200

    Uncommented job logs

--- src/slurm-scripts/check-thor.sh
+++ src/slurm-scripts/check-thor.sh
@@ -4,18 +4,16 @@
 #SBATCH --output=/home-mscluster/smthethwa/slurm-logs/check-thor/%j.out
 #SBATCH --error=/home-mscluster/smthethwa/slurm-logs/check-thor/%j.err
 
-echo 'test'
-#
-# echo ------------------------------------------------------
-# echo -n 'Job is running on node ' $SLURM_JOB_NODELIST
-# echo ------------------------------------------------------
-# echo SLURM: sbatch is running on $SLURM_SUBMIT_HOST
-# echo SLURM: job ID is $SLURM_JOB_ID
-# echo SLURM: submit directory is $SLURM_SUBMIT_DIR
-# echo SLURM: number of nodes allocated is $SLURM_JOB_NUM_NODES
-# echo SLURM: number of cores is $SLURM_NTASKS
-# echo SLURM: job name is $SLURM_JOB_NAME
-# echo ------------------------------------------------------
+echo ------------------------------------------------------
+echo -n 'Job is running on node ' $SLURM_JOB_NODELIST
+echo ------------------------------------------------------
+echo SLURM: sbatch is running on $SLURM_SUBMIT_HOST
+echo SLURM: job ID is $SLURM_JOB_ID
+echo SLURM: submit directory is $SLURM_SUBMIT_DIR
+echo SLURM: number of nodes allocated is $SLURM_JOB_NUM_NODES
+echo SLURM: number of cores is $SLURM_NTASKS
+echo SLURM: job name is $SLURM_JOB_NAME
+echo ------------------------------------------------------
 #
 # cd $SLURM_SUBMIT_DIR
 #
