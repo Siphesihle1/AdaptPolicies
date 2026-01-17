e7636dc50b77bfe6611c9285334fdbf1a1874924
Siphesihle Mthethwa <siphesihlemthetwa@M-BBD-SipM.local>
2026-01-18 01:46:21 +0200

    Removed nographics and modified test script

--- helper-scripts/run-sim.sh
+++ helper-scripts/run-sim.sh
@@ -5,5 +5,5 @@ SIM_VERSION_NUMBER=${VIRTUALHOME_SIM_VERSION##*/v}
 nohup xvfb-run -a \
   --server-args="-screen 0 640x480x24" \
   ${VIRTUALHOME_ROOT}/simulation/linux_exec.*${SIM_VERSION_NUMBER}.x86_64 \
-  -nographics -batchmode -http-port=$SIM_PORT -logFile Player.log > $HOME/vh_sim.log 2>&1 &
+  -batchmode -http-port=$SIM_PORT -logFile Player.log > $HOME/vh_sim.log 2>&1 &
 
