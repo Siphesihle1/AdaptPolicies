b13f1175696b6586ff4f5869381961b86ece8e69
Siphesihle Mthethwa <siphesihlemthetwa@M-BBD-SipM.local>
2026-01-18 15:23:58 +0200

    Fixed syntax error

--- src/slurm-scripts/test-vh-sim-exec.sh
+++ src/slurm-scripts/test-vh-sim-exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=bigbatch
-#SBATCH --nodelist mscluster46,mscluster53,mscluster56
-#SBATCH --nodes 1
+#SBATCH --nodelist=mscluster46,mscluster53,mscluster56,mscluster77
+#SBATCH --nodes=1
 #SBATCH --job-name=test-vh-sim-exec
 #SBATCH --output=/home-mscluster/smthethwa/slurm-logs/test-vh-sim-exec/%j.out
 #SBATCH --error=/home-mscluster/smthethwa/slurm-logs/test-vh-sim-exec/%j.err
