cd214f1e00ec411c967c9d243abdf6108dffdfa3
Siphesihle Mthethwa <siphesihlemthetwa@M-BBD-SipM.local>
2026-01-18 13:22:58 +0200

    Fixed script

--- helper-scripts/start_ollama.sh
+++ helper-scripts/start_ollama.sh
@@ -4,7 +4,8 @@ OLLAMA_PREFIX=~/.local/ollama
 OLLAMA_EXEC=$OLLAMA_PREFIX/bin/ollama
 
 # Run the background, in a way that survives to the next step
-nohup OLLAMA_DEBUG=1 $OLLAMA_EXEC serve > $HOME/ollama.log 2>&1 &
+export OLLAMA_DEBUG=1
+nohup $OLLAMA_EXEC serve > $HOME/ollama.log 2>&1 &
 
 # Block using the ready endpoint
 time curl --retry 360 --retry-connrefused --retry-delay 2 -sf $OLLAMA_HOST
