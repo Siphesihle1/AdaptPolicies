651292ec1376284ce9aab1b56eabc6189bdab205
Siphesihle Mthethwa <siphesihlemthetwa@M-BBD-SipM.local>
2026-01-29 17:37:28 +0200

    Changed node

--- src/slurm-scripts/test-vh-sim-exec.sh
+++ src/slurm-scripts/test-vh-sim-exec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=bigbatch
-##SBATCH -w mscluster42
+##SBATCH -w mscluster77
 #SBATCH --job-name=test-vh-sim-exec
 #SBATCH --output=/home-mscluster/smthethwa/slurm-logs/test-vh-sim-exec/%j.out
 #SBATCH --error=/home-mscluster/smthethwa/slurm-logs/test-vh-sim-exec/%j.err
