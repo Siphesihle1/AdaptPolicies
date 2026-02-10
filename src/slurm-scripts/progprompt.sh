176aaf83108c6c220ed12c5fb81cccb7199d44fb
Siphesihle1 <percysiphe@gmail.com>
2026-02-10 22:32:11 +0200

    Added new rules for generating minimal plans

--- src/slurm-scripts/progprompt.sh
+++ src/slurm-scripts/progprompt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=bigbatch
-##SBATCH -w mscluster87
+#SBATCH -w mscluster87
 #SBATCH --job-name=progprompt
 #SBATCH --output=/home-mscluster/smthethwa/slurm-logs/progprompt/%j.out
 #SBATCH --error=/home-mscluster/smthethwa/slurm-logs/progprompt/%j.err
